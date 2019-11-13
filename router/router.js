460bd037cd5b2af3356e6d28b99191a0438b7bc0
Daniel <kyungmin.dan.lim@gmail.com>
2019-11-13 14:18:35 -0800

    Initial commit

--- router/router.js
+++ router/router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const messengerController = require('../controller/messengerControl');
+const profileController = require('../controller/profileControl');
 const router = express.Router();
 
 router.get('/messenger', messengerController.getUserList)
 router.get('/getMessage/:id', messengerController.getMessages)
+router.get('/profile', profileController.getProfile)
 module.exports = router;
