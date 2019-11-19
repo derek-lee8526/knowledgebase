4e1ffc345923b3c850bf27478cf55274c556e79e
brandon <leeyongl5263@gmail.com>
2019-11-19 15:37:31 -0800

    Added table existence checking and create

--- utils/database.js
+++ utils/database.js
@@ -10,11 +10,34 @@ const mysql = require('mysql2')
 // });
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: "192.168.64.2",
     user: 'brandon',
     database: 'knowledgebase',
-    password: 'brandon'
+    password: 'brandon',
 });
 
+pool.query('SHOW TABLES LIKE "message"', (error, results) => {
+    if (error) {
+        return console.log(error);
+    }
+
+    if (results.length <= 0) {
+        console.log('success', results);
+        pool.query('CREATE TABLE message ' +
+            '(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,' +
+            'sender VARCHAR(30) NOT NULL, ' +
+            'receiver VARCHAR(30) NOT NULL, ' +
+            'body VARCHAR(50),' +
+            'messageTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)', (err, result) => {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log(result);
+            });
+    } else if (results) {
+        console.log(results);
+    }
+
+});
 
 module.exports = pool.promise();
