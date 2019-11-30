a6732f6b497b61367b6ef7c09e70b7d451e0b3c0
Daniel <kyungmin.dan.lim@gmail.com>
2019-11-30 13:02:25 -0800

    page 7 first commit

---
+++ controller/otherUserProfileController.js
@@ -0,0 +1,50 @@
+let otherUserProfileModel = require('../model/otherUserProfileModel');
+
+exports.getOtherUserData = (req, res, next) => {
+    let userData = otherUserProfileModel.getOtherUserProfile(req.query.id);
+    let userPosts = otherUserProfileModel.getOtherUserPosts(req.query.id);
+    // let userMessages = homepageModel.getUserMessages(req.query.id);
+    let postData = otherUserProfileModel.getOtherUserLatestPosts();
+    let postReplies = otherUserProfileModel.getOtherUserReplies(req.query.id);
+    console.log(userPosts)
+    // res.render('homepage', {userProfile: userData, userPosts: userPosts, userMessages: userMessages, latestPosts: postData, postReplies: postReplies, homepageCSS: true})
+    //i need to change 'homepage'.
+    res.render('otherUserProfile', {otherUserProfile: userData, otherUserPosts: userPosts, otherUserLatestPosts: postData, otheruserPostReplies: postReplies, otherUserProfileCSS: true})
+}
+
+// exports.postAddPost = (req,res,next) => {
+//     let p_subject = req.body.subject;
+//     let p_detail = req.body.detail;
+//     let p_topic = req.body.topic;
+    
+//     let pOject = {
+//        subject: p_subject,
+//        detail: p_detail,
+//        topic: p_topic,
+//        //postTime: 
+//     }
+ 
+//     peopleModel.add(pOject);
+//     res.redirect(301, '/homepage');
+// }
+
+// exports.postAddReply = (req,res,next) => {
+//     let p_comment = req.body.comment;
+    
+//     let pOject = {
+//        subject: p_comment,
+//        //postTime: 
+//     }
+ 
+//     peopleModel.add(pOject);
+
+//     // i need to change 'homepage' here.
+//     res.redirect(301, '/homepage');
+// }
+
+exports.getOtherUserReplies = (req, res, next) => {
+    console.log(req.query.id);
+    let replyData = otherUserProfileModel.getOtherUserReplies(req.query.id);
+
+    res.send(JSON.stringify(replyData));
+}
--- router/router.js
+++ router/router.js
@@ -3,6 +3,7 @@ const messengerController = require('../controller/messengerControl');
 const profileController = require('../controller/profileControl');
 const loginController = require('../controller/loginController');
 const homepageController =require('../controller/homepageController');
+const otherUserProfileController =require('../controller/otherUserProfileController');
 const router = express.Router();
 
 router.get('/messenger', messengerController.getUserList)
@@ -19,6 +20,12 @@ router.get('/homepage', homepageController.getHomepageData)
 router.post('/homepage', homepageController.postAddPost)
 router.post('/homepage', homepageController.postAddReply)
 router.get('/getReply/:id', homepageController.getReplies)
+
+router.get('/otherUserProfile', otherUserProfileController.getOtherUserData)
+// router.post('/homepage', otherUserProfileController.postAddPost)
+// router.post('/homepage', otherUserProfileController.postAddReply)
+//i need to change this.
+router.get('/getReply/:id', otherUserProfileController.getOtherUserReplies)
 
 
 module.exports = router;
